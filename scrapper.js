3d478946ef6ae7565de2d4348c6115cc8c6bd6f4
Devang Tomar <32426600+devangtomar@users.noreply.github.com>
2022-12-25 15:42:59 +0530

    Adding more fields to the scrapper

--- scrapper.js
+++ scrapper.js
@@ -1,12 +1,50 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-const url = 'https://www.rottentomatoes.com/';
+const URL = 'https://www.rottentomatoes.com/';
 
-request(url, function (error, response, html) {
-    if (! error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const title = $('title').text();
-        console.log(title);
+request(URL, function (error, response, html) {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
+
+    // Example: Get the title and rating of the first movie on the homepage
+    const firstMovieTitle = $('.mb-movie:nth-of-type(1) h3 a').text();
+    const firstMovieRating = $('.mb-movie:nth-of-type(1) .tMeterScore').text();
+
+    console.log(`Title: ${firstMovieTitle}`);
+    console.log(`Rating: ${firstMovieRating}\n`);
+
+    // Example: Get the titles and ratings of all movies on the homepage
+    const movieTitles = [];
+    const movieRatings = [];
+
+    $('.mb-movie h3 a').each(function (i, elem) {
+      movieTitles[i] = $(this).text();
+    });
+
+    $('.mb-movie .tMeterScore').each(function (i, elem) {
+      movieRatings[i] = $(this).text();
+    });
+
+    for (let i = 0; i < movieTitles.length; i++) {
+      console.log(`Title: ${movieTitles[i]}`);
+      console.log(`Rating: ${movieRatings[i]}\n`);
     }
+
+    // Example: Save the titles and ratings of all movies on the homepage to a file
+    const data = [];
+
+    $('.mb-movie').each(function (i, elem) {
+      const title = $(this).find('h3 a').text();
+      const rating = $(this).find('.tMeterScore').text();
+
+      data.push({ title, rating });
+    });
+
+    fs.writeFile('movies.json', JSON.stringify(data), function (err) {
+      if (err) throw err;
+      console.log('Data saved to file.');
+    });
+  }
 });
